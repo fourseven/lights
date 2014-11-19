0aae08f3b1455464a4c80d15889173567ffbd909
Mathew Hartley <me@mathewhartley.com>
2014-11-19 22:43:24 +1300

    Clock definitions seem to work

--- clock.cpp
+++ clock.cpp
@@ -1,7 +1,16 @@
 #include "clock.h"
 
-// SoftI2C   i2c(A4, A5); // assign pins to SDA and SCL
-// DS3232RTC rtc(i2c);
+#include <SoftI2C.h> // initialise required libraries
+#include <DS3232RTC.h>
+#if (ARDUINO >= 100)
+ #include <Arduino.h>
+#else
+ #include <WProgram.h>
+ #include <pins_arduino.h>
+#endif
+
+SoftI2C i2c(A4, A5); // assign pins to SDA and SCL
+DS3232RTC rtc(i2c);
 
 void Clock::setup() {
 
--- clock.h
+++ clock.h
@@ -1,18 +1,5 @@
 #ifndef CLOCK_H
 #define CLOCK_H
-#include <EEPROM24.h>
-#include <I2CMaster.h>
-#include <SoftI2C.h>
-#include <RTC.h>
-#include <DS3232RTC.h>
-
-
-// #if (ARDUINO >= 100)
-//  #include <Arduino.h>
-// #else
-//  #include <WProgram.h>
-//  #include <pins_arduino.h>
-// #endif
 
 class Clock {
   public:
